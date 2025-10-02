67ea045373a45269ae57fab0eaca52675d51e0b0
DYLANeay <dylan.eray6@gmail.com>
2025-10-02 15:25:08 +0200

    simple counter app

---
+++ vue-infradon2/src/components/AppCounter.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+
+const decrement = () => {
+  count.value--;
+};
+</script>
+
+<template>
+  <div>
+    <p>compteur : {{ count }}</p>
+    <button @click="increment">increment</button>
+    <button @click="decrement">decrement</button>
+  </div>
+</template>
+
+<style scoped>
+button {
+  margin-right: 10px;
+  background-color: black;
+  color: white;
+  border: none;
+  padding: 10px;
+}
+</style>
