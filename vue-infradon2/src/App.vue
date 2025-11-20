d92aa772f5803f11ba371fbf6ddc227da3bf848a
DYLANeay <dylan.eray6@gmail.com>
2025-11-20 16:06:38 +0100

    init comment component

--- vue-infradon2/src/App.vue
+++ vue-infradon2/src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import MainComponent from './components/MainComponent.vue';
+import MainComponent from "./components/MainComponent.vue";
 </script>
-
 <template>
-  <MainComponent />
+	<MainComponent />
 </template>
---
+++ vue-infradon2/src/components/CommentAdd.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+declare interface Comment {
+	content: string;
+	author: string;
+	book_id: string;
+}
+interface Props {
+	storage: any;
+	isOnline?: boolean;
+}
+const props = defineProps<Props>();
+const emit = defineEmits<{
+	close: [];
+	commentAdded: [];
+}>();
+</script>
+
+<template>
+	<div></div>
+</template>
+
+<style scoped></style>
